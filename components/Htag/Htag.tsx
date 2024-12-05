f12626351f8f1769edfd2f64250dfb5c5af24f46
tatnik <ptn48@yandex.ru>
2024-12-06 00:21:24 +0300

    add classname

--- components/Htag/Htag.tsx
+++ components/Htag/Htag.tsx
@@ -1,6 +1,6 @@
 import { HtagProps } from './Htag.props';
 import styles from './Htag.module.css';
-
+import cn from 'classnames';
 
 export const Htag = (props: HtagProps): JSX.Element =>{
   const {tag, children} = props;
