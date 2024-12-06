cad706014fe1c447049db0f82195f315b25319df
tatnik <ptn48@yandex.ru>
2024-12-06 13:12:48 +0300

    add svgr

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Button, Htag} from '../components';
+import Logo from '../public/next.svg';
+
 
 export default function Home() {
   return (
     <>
     <Htag tag='h1'>Текст</Htag>
+    <Logo/>
     <Button appearance='primary'>Кнопка</Button>
-    <Button appearance='ghost'>Кнопка2</Button>
+    <Button appearance='ghost'> кнопка 2</Button>
     </>
   );
 }
