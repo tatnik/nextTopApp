9f8ebcfabc706f2a8644b969e0655ecae20ce9ac
tatnik <ptn48@yandex.ru>
2024-12-15 11:56:32 +0300

    export const firstMenu

--- app/components/Menu/ServerMenu.tsx
+++ app/components/Menu/ServerMenu.tsx
@@ -7,14 +7,14 @@ import BooksIcon from '@/public/icons/books.svg';
 import ProductsIcon from '@/public/icons/products.svg';
 import { Menu } from './Menu';
 
-export async function ServerMenu() {
-  const firstMenu: FirstLevelMenuItem[] = [
-    { route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
-    { route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
-    { route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
-    { route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products },
-  ];
+export const firstMenu: FirstLevelMenuItem[] = [
+  { route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
+  { route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
+  { route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
+  { route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products },
+];
 
+export async function ServerMenu() {
   const firstCategory = firstMenu[0].id;
   const secondMenu = await getMenu(firstCategory);
   return (
